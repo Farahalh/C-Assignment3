6741adfec86b49c9a102b9e0e27bdb484468e2b5
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-09-20 13:16:05 +0200

    Creating public class Person with private fields and public properties with get and set

---
+++ C#Assignment3/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Assignment3
+{
+    public class Person
+    {
+        private int _age;
+        private string _fName;
+        private string _lName;
+        private double _height;
+        private double _weight;
+
+        public int Age { get { return _age; } set { _age = value; } }
+        public string FName { get { return _fName; } set { _fName = value; } }
+        public string LName { get { return _lName; } set { _lName = value; } }
+        public double Height { get { return _height; } set { _height = value; } }
+        public double Weight { get { return _weight; } set { _weight = value; } }
+    }
+}
