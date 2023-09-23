2cff3fbac53cf8290c72a6663a9e8ce6fb98b740
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-09-23 21:18:45 +0200

    creating abstract class UserError, abstract method UEMessage with return type string, creating regular class NumericInputError who inherits from UserError

---
+++ C#Assignment3/UserError.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Assignment3
+{
+    abstract class UserError
+    {
+        public abstract string UEMessage();
+    }
+
+    class NumericInputError : UserError
+    {
+        public override string UEMessage() 
+        {
+            return "You tried to use a numeric input in a text only field. This fired and error!";
+        }
+    }
+}
