82e2dd6006181346e54176bd5af75f1acd50c88f
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-09-23 08:14:47 +0200

    Implementing validations
    Pausing first part of assignment, moving on to next part.

--- C#Assignment3/Person.cs
+++ C#Assignment3/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +15,63 @@ namespace C_Assignment3
         private double _height;
         private double _weight;
 
-        public int Age { get { return _age; } set { _age = value; } }
-        public string FName { get { return _fName; } set { _fName = value; } }
-        public string LName { get { return _lName; } set { _lName = value; } }
+        public int Age 
+        { 
+            get 
+            { return _age; } 
+            set 
+            { 
+                if (_age > 0)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Age must be greater than 0.");
+                }
+            } 
+        }
+
+        //Commenting out below as it needs a UI in ASP.NET
+        //[Required(ErrorMessage = "This is a required field.")]
+        //[Range(2,10, ErrorMessage = "Name must be between 2 and 10 letters.")]
+        public string FName 
+        { 
+            get 
+            { return _fName; } 
+            set 
+            {
+                if (_fName.Length < 10)
+                {
+                    _fName = value;
+                }
+
+                else
+                {
+                Console.WriteLine("Name must be greater than 2.");
+                }
+            } 
+        }
+        public string LName 
+        { 
+            get 
+            { return _lName; } 
+            set 
+            { 
+                _lName = value; 
+            } 
+        }
         public double Height { get { return _height; } set { _height = value; } }
         public double Weight { get { return _weight; } set { _weight = value; } }
+
+        public void DisplayFullName()
+        {
+            Console.WriteLine($"Full Name: {FName} {LName}");
+        }
+
+        public void DisplayPersonAttributes()
+        { 
+            Console.WriteLine($"You are {Age} years, {Height}cm tall and weigh {Weight}kg."); 
+        }
     }
 }
--- C#Assignment3/Program.cs
+++ C#Assignment3/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
+            Person person = new Person();
+            Console.WriteLine("What is your first name?");
+            person.FName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            person.LName = Console.ReadLine();
+            person.DisplayFullName();
 
+            Console.WriteLine("How old are you?");
+            person.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is your height?");
+            person.Height = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is your weight?");
+            person.Weight = int.Parse(Console.ReadLine());
+            person.DisplayPersonAttributes();
         }
     }
 }
